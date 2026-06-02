cf2fe1b3ef55563ccae88737d22140e54873d570
Tersom <ibnua0291@gmail.com>
2026-06-02 10:53:26 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+price = 100000
+print(price)
+
+discount = 0.5
+print(discount)
+
+price = price * discount
+print(price)
+
+product_name = "baso"
+print(product_name)
+
+is_discount = False
+print(is_discount)
+
